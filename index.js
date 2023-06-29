9e9c7ef772cfe124139417445e89299d3787be90
KIPROTYCH <kiprotych@gmail.com>
2023-06-30 00:19:51 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const length = word.length;
+
+  // iterate from the beginning to the middle
+  for (let i = 0; i < length / 2; i++) {
+    const j = length - 1 - i;
+    const startChar = word[i];
+    const endChar = word[j];
+
+    if (startChar !== endChar) {
+      return false;
+    }
+  }
+
+  return true;
+
 }
 
 /* 
   Add your pseudocode here
+  This means that the first letter is the same as the last letter, and the second letter is 
+  the same as the first letter it returns true else it will return false.
+
+  iterate from the beginning to the middle
+  check each letter to the corresponding letter from the end
+
+  returns true
 */
 
 /*
